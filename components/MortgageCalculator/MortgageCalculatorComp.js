55724d5610b35cc0c2e84fb15092371373470277
MutazHelal <42630919+MutazHelal@users.noreply.github.com>
2022-06-21 00:49:45 -0400

    fix

--- components/MortgageCalculator/MortgageCalculatorComp.js
+++ components/MortgageCalculator/MortgageCalculatorComp.js
@@ -401,7 +401,7 @@ export default function MortgageCalculatorComp() {
                         newState.propertyTax = Number(e.target.value.replaceAll(",", ""));
                         updateCalculatorState(newState);
                       }} />
-                    <span className="field-unit">per years</span>
+                    <span className="field-unit">per year</span>
                   </div>
                 </div>
 
